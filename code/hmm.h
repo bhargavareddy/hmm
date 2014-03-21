27f3065a5a916d791866408686d72b77436ec08c
THIPIREDDY BHARGAVA REDDY <bhargava@osl-07.cse.iitb.ac.in>
2014-03-21 15:20:02 +0530

    Second commit

---
+++ code/hmm.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <utility>
+
+using namespace std;
+
+class hmm
+{
+	map<string,int> loc_s;
+	map<string,int> loc_c;
+	map<string,float> prob_start;			// the start probabilities of state
+	map<pair<int,int>,float> prob_trans;	// The transition probability
+	map<pair<int,int>,float> prob_emis;		// The emission probability
+	vector<int> exec_list;	
+public:
+	hmm(map<string,int> _loc_s, 
+		map<string,int> _loc_c, 
+		map<string,float> _prob_start, 
+		map<pair<int,int>,float> _prob_trans, 
+		map<pair<int,int>,float> _prob_emis);
+
+	void load_obs_list(vector<int> _exec_list);
+
+	/*
+		We need to give some notation to various variables like
+		P(Y/X)     		since it is constant for all t it is same
+		P(Xt/Y1:t) 		this is denoted as prob_t();
+		P(Xt/Y1:t-1) 	This is denoted as prob_t_1();
+ 	*/
+
+
+	float P_O_i(int y,int x);
+	float P_S_i(int x2,int x1);
+	float prob_t(int t);
+	float prob_t_1(int t);
+};
---
+++ code/main.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <utility>
+
+#include "hmm.h"
+
+using namespace std;
+
+
+map<string,int> s_loc;					// To give the location of a string
+map<string,int> o_loc;					// To give the location of the observation in the list 
+map<string,float> start_prob;			// the start probabilities of state
+map<pair<int,int>,float> trans_prob;	// The transition probability
+map<pair<int,int>,float> emis_prob;		// The emission probability
+vector<int> observation_vec;
+
+float give_prob_T(string s1,string s2)
+{
+	int i1 = s_loc[s1];
+	int i2 = s_loc[s2];
+
+	pair<int,int> p = make_pair(i1,i2);
+
+	float result;
+	result = trans_prob[p];
+
+	return result;
+}
+
+float give_prob_E(string s1,string s2)
+{
+	int i1 = s_loc[s1];
+	int i2 = o_loc[s2];
+
+	pair<int,int> p = make_pair(i1,i2);
+
+	float result;
+	result = emis_prob[p];
+
+	return result;
+}
+
+int main()
+{
+	int s;	// number of states
+	int o;	// number of possible observations something can make
+	int i,j; 	// Temporary variable used in for statements
+	string temp_s = "";
+	string temp_o = "";
+	float prob_s;
+	float total_prob;
+
+
+	cout<<"Give the number of states: ";
+	cin>>s;
+
+	cout<<"Give the number of observations: ";
+	cin>>o;
+	
+	// The values of s,o are available with us
+
+	// Assuming that each state is represented by a string
+	// Taking the input of all the states
+
+	cout<<"Please give me the states: "<<endl;
+	vector<string> state_vec;
+
+	for(i=0;i<s;i++)
+	{
+		cout<<"state "<<i<<": ";
+		cin>>temp_s;
+		state_vec.push_back(temp_s);
+
+		s_loc[temp_s] = i;
+	}
+
+	cout<<"Please give me all the possile observations: "<<endl;
+	vector<string> obs_vec;
+
+	for(i=0;i<o;i++)
+	{
+		cout<<"state "<<i<<": ";
+		cin>>temp_o;
+		obs_vec.push_back(temp_o);
+
+		o_loc[temp_o] = i;
+	}
+
+	//Creating a map for start probabilities
+
+	cout<<"Please give me the probabilities for each of the states: "<<endl;
+	
+	total_prob = 0.0;
+	for(i=0;i<s;i++)
+	{
+		cout<<state_vec[i]<<": ";
+		cin>>prob_s;
+		total_prob = total_prob + prob_s;
+		if(total_prob < 1.0 || i== (s - 1))
+			{
+				if(i == (s - 1))
+					start_prob[state_vec[i]] = 1.0 - (total_prob - prob_s);
+				else
+					start_prob[state_vec[i]] = prob_s;
+			}
+		else
+			{
+				cout<<"The total probability is exceeding 1. Program Exiting...."<<endl;
+				exit(1);
+			}
+
+	}
+
+	//// Taking the inputs for transitions..
+	cout<<"Give the inputs for transition probabilities: \n";
+	pair<int,int> my_pair;
+	for(i=0;i<s;i++)
+	{
+		total_prob = 0.0;
+		for(j=0;j<s;j++)
+		{
+			cin>>prob_s;
+			total_prob = total_prob + prob_s;
+			my_pair = make_pair(i,j);
+			if(total_prob < 1 || j==(s - 1))
+			{
+				if(j == (s - 1))
+				{
+					trans_prob[my_pair] = 1.0 - (total_prob - prob_s);
+				}
+				else
+				{
+					trans_prob[my_pair] = prob_s;
+				}
+			}
+			else
+			{
+				cout<<"The total probability is exceeding 1. Program Exiting...."<<endl;
+				exit(1);
+			}
+		}
+	}
+
+	cout<<"Give the inputs for emission probabilities: \n";
+	for(i=0;i<s;i++)
+	{
+		total_prob = 0.0;
+		for(j=0;j<o;j++)
+		{
+			cin>>prob_s;
+			total_prob = total_prob + prob_s;
+			my_pair = make_pair(i,j);
+			if(total_prob <= 1.0 || j==(o - 1))
+			{
+				if(j == (o - 1))
+				{
+					emis_prob[my_pair] = 1.0 - (total_prob - prob_s);
+				}
+				else
+				{
+					emis_prob[my_pair] = prob_s;
+				}
+			}
+			else
+			{
+				cout<<"The total probability is exceeding 1. Program Exiting...."<<endl;
+				exit(1);
+			}
+		}
+	}
+
+	cout<<"All inputs have been succesfully provided"<<endl;
+
+	/// Now we need to take the values for evaluation
+	/// we need to first take the input of number of observations recorded by the computer
+
+	int obs;
+	cout<<"Please provide the number of oservations: ";
+	cin>>obs;
+
+	cout<<"The observations are: \n";
+
+	for(i=0;i<obs;i++)
+	{
+		cin>>temp_o;
+		observation_vec.push_back(o_loc[temp_o]);
+	}
+
+	hmm HMM =  hmm(s_loc,o_loc,start_prob,trans_prob,emis_prob);
+
+	return 0;
+}
